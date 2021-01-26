6f122f0a4cc5bb1a8cd1f42624543ff191f6a4f4
Lucas Montanari <Lmonty2011@gmail.com>
2021-01-26 10:20:31 -0800

    polished login/logout functionality

--- roundTracker/frontend/main.js
+++ roundTracker/frontend/main.js
@@ -7,7 +7,6 @@ window.addEventListener("DOMContentLoaded", () => {
 // add event listeners 
 function addEventListeners() {
     let form = document.getElementsByClassName("navbar-form navbar-left")[0]
-    console.log(form)
     let login = document.getElementById('login')
     let signup = document.getElementById('signup')
 
@@ -31,23 +30,40 @@ function postUser(form) {
 
 // get the user on login
 function getUser(form) {
-    username = form.name.value
-    fetch(`http://127.0.0.1:3000/login/${username}`)
-    .then(resp => resp.json())
-    .then(obj => {
-        console.log(obj)
-        if (obj['message']) {
-            let alert = document.createElement("div")
-            alert.className="alert alert-danger"
-            alert.textContent = obj['message']
-
-            let body = document.querySelector('body')
-            setTimeout(() => {body.appendChild(alert)}, 500)
-        }else {
+    let login = document.getElementById("login")
+    let signup = document.getElementById('signup')
+    debugger
+    if(login.textContent === "Login"){
+        username = form.name.value
+        fetch(`http://127.0.0.1:3000/login/${username}`)
+        .then(resp => resp.json())
+        .then(obj => {
             console.log(obj)
-            signedIn(obj)
-        }
-    })
+            if (obj['message']) {
+                let alert = document.createElement("div")
+                alert.className="alert alert-danger"
+                alert.textContent = obj['message']
+
+                let body = document.querySelector('body')
+                setTimeout(() => {body.appendChild(alert)}, 500)
+            }else {
+                localStorage.setItem('user', obj.username)
+                login.textContent = 'Logout'
+                signup.style.visibility = "hidden"
+                document.getElementsByClassName('form-control')[0].style.visibility = "hidden"
+                renderCreateRound(localStorage.user)
+            }
+            form.name.value = ""
+        })
+    }else {
+        localStorage.removeItem('user')
+        form.name.value = ""
+        form.name.style.visibility = 'visible'
+        login.textContent = "Login"
+        signup.style.visibility = "visible"
+        // render the welcome page 
+        renderWelcome()
+    }
 }
 
 // render the welcome page
@@ -56,11 +72,11 @@ function renderWelcome() {
 }
 
 // called when the user logs in
-function signedIn(user) {
-    document.getElementById('login').textContent = 'Logout'
-    document.getElementById('signup').style.visibility = "hidden"
-    renderCreateRound(user)
-}
+// function signedIn(user) {
+//     document.getElementById('login').textContent = 'Logout'
+//     document.getElementById('signup').style.visibility = "hidden"
+//     renderCreateRound(user)
+// }
 
 // renders the create round form 
 function renderCreateRound(user) {
